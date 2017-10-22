b4fdefa1b02481fe79370bb1d4a42194493f57a7
Opesanya Adebayo <Bayo@onejsninja.local>
2017-10-22 23:10:33 +0100

    refactored getTweets

--- routes/submitHandle.js
+++ routes/submitHandle.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var getTweets = require("../helpers/twitter/getTweets")
+var getTweets = require("../helpers/getTweets")
 /* GET home page. */
 router.post('/', function(req, res, next) {
     let handle = req.body.twitterHandle;
-    getTweets.processTweets(handle).then(function(filteredTweets){
-        console.log(filteredTweets.length);
+    getTweets.processTweets(handle).then(function(tweets){
+        console.log(tweets.length);
     });
-//   res.render('inputhandle', { title: 'Career Advisor' });
+
 });
 
 module.exports = router;
