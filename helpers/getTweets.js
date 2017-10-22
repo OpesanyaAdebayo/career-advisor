a70b9173d2b427ecdcab0d33b51fab2e17b8e6f5
OpesanyaAdebayo <bayoopesanya@gmail.com>
2017-10-22 23:10:33 +0100

    refactored getTweets

---
+++ helpers/getTweets.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+let Twitter = require('twitter');
+let client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+// This is for error handling
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
+const processTweets = (username) => {
+    return new Promise((resolve, reject) => {
+        let params = {
+            screen_name: username,
+            count: 200,
+            include_rts: false,
+            trim_user: true,
+            exclude_replies: true
+        }
+        let tweets = [];
+        const fetchTweets = (error, newTweets) => {
+            if (error) {
+                return reject(Error(error));
+            }
+            filteredTweets = newTweets.map(function (tweet) {
+                return {
+                    id: tweet.id_str,
+                    language: tweet.lang,
+                    contenttype: 'text/plain',
+                    content: tweet.text.replace('[^(\\x20-\\x7F)]*', ''),
+                    created: Date.parse(tweet.created_at),
+                    reply: tweet.in_reply_to_screen_name != null
+                };
+            });
+            if (newTweets.length > 1) {
+                tweets = tweets.concat(filteredTweets);
+                params.max_id = tweets[tweets.length - 1].id - 1;
+                console.log(params.max_id);
+                client.get('statuses/user_timeline', params, fetchTweets)
+            } else {
+                resolve(tweets);
+            }
+        };
+        client.get('statuses/user_timeline', params, fetchTweets);
+    });
+    // client.get('statuses/user_timeline', params, fetchTweets);
+}
+
+// processTweets("onejsninja");
+
+module.exports = {
+    processTweets: processTweets
+};
