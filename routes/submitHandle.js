1d641d802d6062942a97ed79a1cdddcf6d4c4057
Opesanya Adebayo <Bayo@onejsninja.local>
2017-10-22 19:56:12 +0100

    can now fetch tweets from users twitter handle

--- routes/submitHandle.js
+++ routes/submitHandle.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var getTweets = require("../helpers/twitter/getTweets")
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('inputhandle', { title: 'Career Advisor' });
+router.post('/', function(req, res, next) {
+    let handle = req.body.twitterHandle;
+    getTweets.processTweets(handle).then(function(filteredTweets){
+        console.log(filteredTweets.length);
+    });
+//   res.render('inputhandle', { title: 'Career Advisor' });
 });
 
 module.exports = router;
