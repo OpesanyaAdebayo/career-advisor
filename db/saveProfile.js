edc484a1891cf72f1177d7ceac764f70d9c71eef
OpesanyaAdebayo <bayoopesanya@gmail.com>
2017-10-23 10:29:42 +0100

    removed db credentials from code

--- db/saveProfile.js
+++ db/saveProfile.js
@@ -1,7 +1,7 @@
 var mongojs = require('mongojs');
 const dbUsername = process.env.DB_USERNAME
 const dbPassword = process.env.DB_PASSWORD
-var db = mongojs('careeradvisor:careeradvisor@ds229465.mlab.com:29465/careeradvisor', ['careeradvisor'])
+var db = mongojs(process.env.MLAB_URI, ['users'])
 
 const saveProfile = (profile) => {
     return new Promise((resolve, reject) => {
