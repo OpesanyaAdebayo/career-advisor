b4376009dd943dc26e72dabdba1408092c2b2300
OpesanyaAdebayo <bayoopesanya@gmail.com>
2017-10-26 18:38:35 +0100

    signup completed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,11 @@
-console.log($("#submit-handle").disabled);
-$("#twitter-handle-form").submit(function (event) {
+$("#twitterHandleForm").submit(function (event) {
+    $("#submitHandle").attr('disabled', 'disabled');
     let processedHandle;
     let rawFormInput = $("#twitterHandle").val();
     processedHandle = rawFormInput.toLowerCase();
-    $.post("/submitHandle", {handle: processedHandle}, function(data) {
+    $.post("/submitHandle", {
+        handle: processedHandle
+    }, function (data) {
         console.log(data);
     })
     event.preventDefault();
@@ -11,9 +13,40 @@ $("#twitter-handle-form").submit(function (event) {
 
 $("#twitterHandle").keyup(function () {
     if ($(this).val().length >= 1 && $(this).val().charAt(0) != "@") {
-        $("#submit-handle").removeAttr('disabled');
+        $("#submitHandle").removeAttr('disabled');
     }
-    if($(this).val().length < 1) {
-        $("#submit-handle").attr('disabled', 'disabled');
+    if ($(this).val().length < 1) {
+        $("#submitHandle").attr('disabled', 'disabled');
+        $("#signup").attr('disabled', 'disabled');
     }
+})
+
+$("#signup").click(function () {
+    $("#loginContainer").hide();
+    $("#signupContainer").show();
+})
+
+$("#login").click(function () {
+    $("#signupContainer").hide();
+    $("#loginContainer").show();
+})
+
+$("#loginForm").submit(function (event) {
+    $("#loginButton").attr('disabled', 'disabled');
+    $("#signup").hide();
+    $("small").hide();
+    $.post("/auth/login", $("#loginForm").serialize(), function (data) {
+        console.log(data);
+    })
+    event.preventDefault();
+})
+
+$("#signupForm").submit(function (event) {
+    $("#signupButton").attr('disabled', 'disabled');
+    $("#login").hide();
+    $("small").hide();
+    $.post("/auth/signup", $("#signupForm").serialize(), function (data) {
+        console.log(data);
+    })
+    event.preventDefault();
 })
