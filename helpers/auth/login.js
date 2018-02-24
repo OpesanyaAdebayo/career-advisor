32cc80bc52873d60cebe3e7ec6dcef56d3ec4ffa
Opesanya Adebayo <Bayo@onejsninja.local>
2018-02-25 00:15:48 +0100

    removed login error

--- helpers/auth/login.js
+++ helpers/auth/login.js
@@ -1,30 +1,30 @@
-let mongojs =  require('mongojs');
-let bcrypt = require('bcryptjs');
-let db = mongojs(process.env.MLAB_URI, ['users']); // Replace with credentials from .env file
-let salt = bcrypt.genSaltSync(10);
-let errormsg;
+// let mongojs =  require('mongojs');
+// let bcrypt = require('bcryptjs');
+// let db = mongojs(process.env.MLAB_URI, ['users']); // Replace with credentials from .env file
+// let salt = bcrypt.genSaltSync(10);
+// let errormsg;
 
-const userLogin = (formInput) => {
-    return new Promise((resolve, reject) => {
-        db.users.findOne({
-            email: formInput.email
-        }, (err, profile) => {
-            if (err) {
-                reject(Error(err));
-            } else if (profile === null) {
-                errormsg = "This email address has not been registered.";
-                resolve(errormsg);
-            } else {
-                if(bcrypt.compareSync(formInput.password, profile.password)) {
-                    resolve("User Succesfully logged in.");
-                }
-               else resolve("Incorrect password.");
-            }
-        });
-    });
-};
+// const userLogin = (formInput) => {
+//     return new Promise((resolve, reject) => {
+//         db.users.findOne({
+//             email: formInput.email
+//         }, (err, profile) => {
+//             if (err) {
+//                 reject(Error(err));
+//             } else if (profile === null) {
+//                 errormsg = "This email address has not been registered.";
+//                 resolve(errormsg);
+//             } else {
+//                 if(bcrypt.compareSync(formInput.password, profile.password)) {
+//                     resolve("User Succesfully logged in.");
+//                 }
+//                else resolve("Incorrect password.");
+//             }
+//         });
+//     });
+// };
 
 
-module.exports = {
-    userLogin: userLogin
-};
+// module.exports = {
+//     userLogin: userLogin
+// };
